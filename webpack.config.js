7a3f90f6c6334b3ee44ef2dc8960d1b52a8e7d15
Matt 3 <not.mattowen@gmail.com>
2024-03-04 09:35:39 -0500

    :wolf: publish to docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   context: __dirname + "/src",
   entry: "./index.js",
   output: {
-    path: __dirname + '/dist',
+    path: __dirname + '/docs',
     filename: "airmaxdotcom.js",
     libraryTarget: 'var',
     library: 'airmaxdotcom',
