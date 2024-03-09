311e98f957828e3965762ef78e85906f7efae5c5
Matt 3 <not.mattowen@gmail.com>
2024-03-08 22:09:07 -0500

    :frog: infinity scroll text

--- src/ScrollingText.js
+++ src/ScrollingText.js
@@ -22,7 +22,6 @@ export class ScrollingText extends HTMLElement {
     this.canvas.style.width = "100%";
 
     this.appendChild(this.canvas);
-
     this.app = new ScrollingTextApp(
       this.canvas,
       this.text,
@@ -71,8 +70,19 @@ class ScrollingTextApp {
   }
 
   update() {
-    const w = Math.max(this.blurb.width, 1);
-    const t = (new Date() / 9.5) % this.blurb.width;
+
+    let h = this.el.height;
+    const ctx = this.context;
+    ctx.save();
+    ctx.font = "bold italic " + (h-10) + "px Franie";;
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "yellow";
+    ctx.fillStyle = "yellow";
+    let box = ctx.measureText(this.blurb.text);
+    ctx.restore();
+
+    const w = Math.max(box.width, 1);
+    const t = (new Date() / 9.5) % w;
     this.offset = Math.floor(t);
   }
 
@@ -98,11 +108,10 @@ class ScrollingTextApp {
     ctx.strokeStyle = "yellow";
     ctx.fillStyle = "yellow";
     let box = ctx.measureText(this.blurb.text);
-    ctx.strokeText(this.blurb.text, x - 2*box.width, y);
+    console.log(box.width);
     ctx.strokeText(this.blurb.text, x - box.width, y);
     ctx.strokeText(this.blurb.text, x, y);
     ctx.strokeText(this.blurb.text, x + box.width, y);
-    ctx.strokeText(this.blurb.text, x + 2*box.width, y);
     ctx.restore();
   }
 }
