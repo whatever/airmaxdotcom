fb0060f7d9887acd4d66e5b06a4f0eec3c4bf096
Matt 3 <not.mattowen@gmail.com>
2024-03-15 16:35:03 -0400

    :rabbit2: le chat

--- src/index.js
+++ src/index.js
@@ -27,10 +27,13 @@ customElements.define("scrolling-text", ScrollingText);
 export function clickThrough() {
   document.body.classList.remove("splash");
   document.body.classList.add("no-splash");
+  window.removeEventListener("click", clickThrough);
+  window.removeEventListener("touchstart", clickThrough);
+  window.removeEventListener("wheel", clickThrough);
 }
 window.addEventListener("load", function() {
-  window.addEventListener("wheel", clickThrough);
   window.addEventListener("click", clickThrough);
   window.addEventListener("touchstart", clickThrough);
+  window.addEventListener("wheel", clickThrough);
 });
 
